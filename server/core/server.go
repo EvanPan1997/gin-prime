2b791817b2a3f975e5bd39e347567d6d545d4fc6
Evan.Pan <panyf_97@163.com>
2024-06-24 11:25:04 +0800

    add: 跨域中间件

--- server/core/server.go
+++ server/core/server.go
@@ -4,10 +4,12 @@ import (
 	"github.com/fvbock/endless"
 	"github.com/gin-gonic/gin"
 	"log"
+	"main/middleware"
 )
 
 func RunServer() {
 	r := gin.Default()
+	r.Use(middleware.Cors())
 
 	server := endless.NewServer(":8080", r)
 	err := server.ListenAndServe()
