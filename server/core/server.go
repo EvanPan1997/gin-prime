ac8499c9f985454117272f05ac3d3ca6091a205e
Evan.Pan <panyf_97@163.com>
2024-07-01 17:20:00 +0800

    cleanup

--- server/core/server.go
+++ server/core/server.go
@@ -3,18 +3,11 @@ package core
 import (
 	"github.com/fvbock/endless"
 	"github.com/gin-gonic/gin"
-	"go.uber.org/zap"
 	"log"
-	"main/global"
 	"main/middleware"
-	"strconv"
 )
 
 func RunServer() {
-	for i := 0; i < 100000; i++ {
-		global.GpLogger.With(zap.String("key", strconv.Itoa(i))).Info("info test")
-	}
-
 	r := gin.Default()
 	r.Use(middleware.Cors())
 
